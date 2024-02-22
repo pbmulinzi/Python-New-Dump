1d5b6dedccb5fa7b26d72ef136219402cb61a457
pbmulinzi <pbmulinzi@gmail.com>
2024-02-22 16:05:59 +0300

    practice

---
+++ new.py
@@ -0,0 +1,9 @@
+#Strings
+first = 'Peter'
+last = 'Mulinzi'
+string = f'{first} {last} is my name.'
+print(string)
+print(string.upper())
+print(string.lower())
+print(len(string))
+print(string[0:])
